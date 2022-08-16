e30d43a4e4a9f19aeb70d7c61532967afe58c84f
codeperson <84794219+codepers0n@users.noreply.github.com>
2022-08-16 22:44:39 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
+     'myweb.apps.MywebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
