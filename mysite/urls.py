88fc6f45fce6b658bcd42c5d4be3a2f7651d0819
codeperson <84794219+codepers0n@users.noreply.github.com>
2022-08-16 22:10:09 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from main import views as main_views
- 
+from django.urls import path, include 
+from myweb import views
+
 urlpatterns = [
-    path('', main_views.index, name="index"),
     path('admin/', admin.site.urls),
+    path('', include('myweb.urls')),
 ]
